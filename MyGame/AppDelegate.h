928a946887741ac4ebd254a6590100b45ac6d8cf
Student <student@11111.local>
2015-10-29 19:19:05 +0800

    Initial Commit

---
+++ MyGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyGame
+//
+//  Created by Student on 15/10/29.
+//  Copyright (c) 2015年 北京渥瑞达科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyGame
+//
+//  Created by Student on 15/10/29.
+//  Copyright (c) 2015年 北京渥瑞达科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
