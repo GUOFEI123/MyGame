9255f18b20f487e605682d43324c36e5f5595009
GUOFEI123 <guo11111fei@163.com>
2015-11-19 15:29:53 +0800

    001
    001

---
+++ MyGame/Game.cpp
@@ -0,0 +1,9 @@
+//
+//  Game.cpp
+//  MyGame
+//
+//  Created by Student on 15/10/29.
+//  Copyright (c) 2015年 北京渥瑞达科技有限公司. All rights reserved.
+//
+
+#include "Game.h"
---
+++ MyGame/Game.h
@@ -0,0 +1,15 @@
+//
+//  Game.h
+//  MyGame
+//
+//  Created by Student on 15/10/29.
+//  Copyright (c) 2015年 北京渥瑞达科技有限公司. All rights reserved.
+//
+
+#ifndef __MyGame__Game__
+#define __MyGame__Game__
+
+#include <iostream>
+
+
+#endif /* defined(__MyGame__Game__) */
